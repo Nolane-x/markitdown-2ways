7ff40b45ecabe91a882f81311d9c98c6c9b61340
Nolane-x <casioreview20@gmail.com>
2026-09-14 19:49:01 +0700

    H11 apply canonical Black form formatting

--- packages/markitdown/src/markitdown/twoways/formats/pdf/forms.py
+++ packages/markitdown/src/markitdown/twoways/formats/pdf/forms.py
@@ -15,9 +15,7 @@ from pypdf.generic import (
 from .limits import PdfNativeLimits
 from .model import PdfParseError, PdfTextFieldEvidence
 
-_UNSUPPORTED_TEXT_FLAGS = (
-    (1 << 12) | (1 << 13) | (1 << 20) | (1 << 24) | (1 << 25)
-)
+_UNSUPPORTED_TEXT_FLAGS = (1 << 12) | (1 << 13) | (1 << 20) | (1 << 24) | (1 << 25)
 _READ_ONLY_FLAG = 1
 
 

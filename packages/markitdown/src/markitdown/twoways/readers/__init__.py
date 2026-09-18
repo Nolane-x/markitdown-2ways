7794eb0c0a2462f146db467e96f634696c1f122d
Nolane-x <casioreview20@gmail.com>
2026-09-18 17:01:34 +0700

    H19: expose Bing remote-derived reader

--- packages/markitdown/src/markitdown/twoways/readers/__init__.py
+++ packages/markitdown/src/markitdown/twoways/readers/__init__.py
@@ -1,8 +1,13 @@
 from .base import DocumentIRReader
-from .remote import RemoteDerivedLimits, read_wikipedia_snapshot_ir
+from .remote import (
+    RemoteDerivedLimits,
+    read_bing_serp_snapshot_ir,
+    read_wikipedia_snapshot_ir,
+)
 
 __all__ = [
     "DocumentIRReader",
     "RemoteDerivedLimits",
+    "read_bing_serp_snapshot_ir",
     "read_wikipedia_snapshot_ir",
 ]
